3b6fbcafde7ec61abe000825319b12ba6b14ea16
Julien Gargot <julien@g-u-i.net>
2019-03-01 14:19:27 +0100

    Rename config property

--- group-images.php
+++ group-images.php
@@ -3,14 +3,14 @@
  * Auto Group Pictures
  *
  * @author Julien Gargot <julien@g-u-i.me>
- * @version 1.0.0
+ * @version 1.0.1
  */
 kirbytext::$pre[] = function($kirbytext, $text) {
   $text = preg_replace_callback('!(\(image:([^\)]+)\)\n{0,1})+!is', function($matches) use($kirbytext) {
     $html = $matches[0];
     $images = preg_match_all('/\(image:([^\)]+)\)/i', $html);
     if ( $images > 0 ) {
-      return '<div class="' . c::get('gallery.container', 'gallery') . '" data-count-images="' . $images . '">' . $html . '</div>';
+      return '<div class="' . c::get('kirbytext.gallery.class', 'gallery') . '" data-count-images="' . $images . '">' . $html . '</div>';
     } else {
       return $html;
     }
